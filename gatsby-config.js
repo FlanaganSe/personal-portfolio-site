f825872312155de784e5032a752d3bae86bb2c70
Sean Flanagan <flanspan11@gmail.com>
2021-09-19 22:52:15 -0400

    Adding a fourth blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/sean-flanagan-b25271b1/`,
     // Content of the About Me section
-    about: `Pragmatic optimist with an entrepreneurial spirit. Previously founded and built the product research company Laptologist LLC. Now working for Livegistics, a startup in Detroit MI. In my free time I love playing chess, exploring new technology, distance running, and skiing / snowboarding!`,
+    about: `Pragmatic optimist with an entrepreneurial side. Previously founded and built the product research company Laptologist LLC. Now a software developer at Livegistics in Detroit MI. In my free time I love playing chess, exploring new technology, rooting for Michigan Football (go blue!), distance running, and skiing & snowboarding!`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
