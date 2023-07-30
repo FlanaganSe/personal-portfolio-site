ea806c799fc5f40292dc8322bb54a1d73d5673b4
Sean Flanagan <flanspan11@gmail.com>
2023-07-30 18:29:13 -0500

    Add autoprefixer for tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Layout, Header } from "./shared/components/Layout.tsx";
 import { Home } from "./pages/Home.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <Layout>
+      <Header metadata={data.site.siteMetadata} noBlog={noBlog} />
+
+      <Home />
+    </Layout>
   </React.StrictMode>
 );
