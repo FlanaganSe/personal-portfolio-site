f9e19aa126c81761b3911dc33c13951ffb40b16d
Sean Flanagan <flanspan11@gmail.com>
2023-07-30 13:22:58 -0500

    Additional tailwind setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@
 // const Home = (props: Props) => {
 export const Home = () => {
   return (
-    <div className="left-10">
-      <h1 className="underline">Home</h1>
+    <div className="m-10 p-10 border-4 border-green-500">
+      <h1 className="text-sm lg:text-5xl">Home</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,59 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        serif: [],
+      },
+      colors: {
+        primary: {
+          50: "#feffe7",
+          100: "#fbffc1",
+          200: "#faff87",
+          300: "#feff42",
+          400: "#fff50f",
+          500: "#f0da02",
+          600: "#cfac00",
+          700: "#a57c03",
+          800: "#89610b",
+          900: "#734e10",
+          950: "#442a04",
+        },
+        secondary: {
+          50: "#e5f5ff",
+          100: "#d0ecff",
+          200: "#abd9ff",
+          300: "#78beff",
+          400: "#4490ff",
+          500: "#1a62ff",
+          600: "#0044ff",
+          700: "#0047ff",
+          800: "#003dde",
+          900: "#032ca5",
+          950: "#021864",
+        },
+        gray: {
+          50: "#f6f6f6",
+          100: "#e7e7e7",
+          200: "#d1d1d1",
+          300: "#b0b0b0",
+          400: "#888888",
+          500: "#6d6d6d",
+          600: "#5d5d5d",
+          700: "#4f4f4f",
+          800: "#454545",
+          900: "#3d3d3d",
+          950: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
 };
