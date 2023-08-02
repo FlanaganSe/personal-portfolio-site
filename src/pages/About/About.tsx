67293f7438cb5992591437e529f147b633d0b0ed
Sean Flanagan <flanspan11@gmail.com>
2023-08-02 17:12:13 -0500

    Finishing sidebar with links to navigation pages

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,22 @@
 // const Home = (props: Props) => {
+
+const classes = {
+  container: "block pt-12 flex flex-col",
+  title: "pb-6 md:w-full md:max-w-150 md:p-0",
+  heading:
+    "text-2xl font-xs font-light tracking-widest text-sm text-gray-600 leading-normal uppercase",
+  content: "flex-none text-lg text-gray-600 font-light md:flex-1 md:pl-20",
+};
+
 export const About = () => {
   return (
-    <div>
-      <h1>Hi, I'm Sean Flanagan!</h1>
+    <div className={classes.container}>
+      <h1 className={classes.title}>Hi, I'm Sean Flanagan!</h1>
+      <h2 className={classes.content}>
+        Optimist who likes software, chess, Michigan sports &#12349; (Go blue!),
+        distance running, skiing, and travel! I'm currently open to work and am
+        seeking new and exciting opportunities
+      </h2>
     </div>
   );
 };
--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -1,18 +1,15 @@
 import { NavLink } from "react-router-dom";
 import SFLogo from "../../assets/sf-logo.png";
+import GithubIcon from "../../assets/github.svg";
+import MailIcon from "../../assets/mail.svg";
+import LinkedInIcon from "../../assets/linkedin.svg";
 
 export const Sidebar = () => {
-  console.log(SFLogo);
-
   return (
-    <div className="border-r-2 border-dotted h-full my-10">
-      <ul className="flex flex-col font-bold text-gray-500 space-y-4 text-lg">
-        <NavLink to={SFLogo} className="m-8">
-          <img
-            src={SFLogo}
-            alt="Sean Flanagan logo"
-            className={"bg-contain size-16"}
-          />
+    <div className="border-r-2 flex flex-col justify-between border-dotted h-full py-10">
+      <ul className="flex flex-col font-bold text-gray-400 space-y-4 text-lg">
+        <NavLink to={"/"} className="m-8">
+          <img src={SFLogo} alt="Sean Flanagan logo" className={"size-16"} />
         </NavLink>
         <NavLink to={""} className="pl-8">
           <div>About</div>
@@ -26,6 +23,18 @@ export const Sidebar = () => {
         <NavLink to={"blog"} className="pl-8">
           <div>Blog</div>
         </NavLink>
+      </ul>
+
+      <ul className="flex flex-row justify-between mx-2">
+        <a href="https://github.com/FlanaganSe">
+          <img src={GithubIcon} alt="Link to github" className={"size-6"} />
+        </a>
+        <a href="mailto:flanagansean@gmail.com">
+          <img src={MailIcon} alt="Link to send email" className={"size-6"} />
+        </a>
+        <a href="https://www.linkedin.com/in/sean-flanagan-b25271b1/">
+          <img src={LinkedInIcon} alt="Link to LinkedIn" className={"size-6"} />
+        </a>
       </ul>
     </div>
   );
