446fa122a5ac2c5dcfbf824277bcd30402f848a6
Sean Flanagan <flanspan11@gmail.com>
2024-03-19 18:35:34 -0400

    Testing deploy time

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+// import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { router } from "./pages/routes";
+// import { router } from "./pages/routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>
 );
